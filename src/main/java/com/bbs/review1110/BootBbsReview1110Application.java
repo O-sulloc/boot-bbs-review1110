0b8abb6286a6e2c420a5c0e03e2d15603b3777de
O-sulloc <bujjaf@gmail.com>
2022-11-10 10:47:04 +0900

    init

---
+++ src/main/java/com/bbs/review1110/BootBbsReview1110Application.java
@@ -0,0 +1,13 @@
+package com.bbs.review1110;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootBbsReview1110Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootBbsReview1110Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bbs/review1110/BootBbsReview1110ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbs.review1110;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootBbsReview1110ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
